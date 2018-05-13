7d655c9bd4ad0f18b54f78248234de6c8c018138
Albert Hansrisuk <alhansrisuk@gmail.com>
2018-05-13 10:56:07 -0400

    updated image results to show content from api

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,6 +3,7 @@ import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import axios from 'axios';
+import ImageResults from '../image-results/ImageResults';
 
 class Search extends Component {
     state = {
@@ -20,6 +21,8 @@ class Search extends Component {
         .catch(err => console.log(err));
     });
   };
+
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
 
   render() {
     console.log(this.state.images);
@@ -45,6 +48,8 @@ class Search extends Component {
           <MenuItem value={30} primaryText="30" />
           <MenuItem value={50} primaryText="50" />
         </SelectField>
+        <br />
+        {this.state.images.length > 0 ? (<ImageResults images={this.state.images} />) : null }
       </div>
     );
   }
