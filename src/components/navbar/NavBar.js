a071b5dce12d03c9fd7a7f139cabf07009728c70
Albert Hansrisuk <alhansrisuk@gmail.com>
2018-05-16 11:03:46 -0400

    changed color of navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import {red500} from 'material-ui/styles/colors';
 
-const NavBar = () => <AppBar title="Pixabay Image Search" />
+const NavBar = () => <AppBar title="Pixabay Image Search" 
+                            style={{ backgroundColor: red500 }} />
 
 export default NavBar;
